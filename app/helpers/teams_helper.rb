143869719df3b7b43065fcf43cd44c2f27d9d0b7
Keleigh Lynch <keleigh@Keleigh-Lynchs-MacBook-Pro.local>
2015-01-24 19:55:01 -0500

    so far

---
+++ app/helpers/teams_helper.rb
@@ -0,0 +1,48 @@
+module TeamsHelper
+
+	def generate_teams
+
+		unassigned_students = []
+
+		Student.all.each do |student|
+
+			unassigned_students.push(student.name)
+
+		end
+
+		num_teams = params[:num].to_i > 0 ? params[:num].to_i : 1
+
+		team_size = unassigned_students.count / num_teams
+
+		teams = [ ] 
+
+		until teams.count == num_teams 
+
+			teammates = unassigned_students.sample(team_size)
+
+			teams.push(teammates)
+
+			teammates.each do |student|
+				unassigned_students.delete(student)
+			end
+		end
+
+		if unassigned_students.count > 0
+			teams[teams.length-1].push(unassigned_students)
+		end
+
+		return teams 
+	end
+
+	def save_teams 
+	    Team.destroy_all
+	    if @teams != nil 
+		    @teams.each do |team|
+		      Team.create(members: team.join(', '))
+		    end
+		end
+	end
+end
+
+
+
